140d08a035f464b737d0b3bd5472b00eae0238f5
Scientist90s <yshethwala@gmail.com>
2022-09-06 12:56:07 -0700

    minor vel calc changes

--- scripts/velocity_calc.py
+++ scripts/velocity_calc.py
@@ -112,14 +112,25 @@ class velocity_calc:
     
     def get_velocity_facc(self, tstamp, filtered_acc):
         f = InterpolatedUnivariateSpline(tstamp, filtered_acc, k=1)  # k=1 gives linear interpolation
-        vel = [f.integral(tstamp[0], tstamp[i]) for i in range(len(tstamp))]
-        return vel
+        vel_acc = [f.integral(tstamp[0], tstamp[i]) for i in range(len(tstamp))]
+        return vel_acc
     
     def get_velocity_fpos(self, tstamp, filtered_disp):
+        # dx = np.diff(filtered_disp)
+        # dt = np.diff(tstamp)
+        # vel_pos = dx/dt
+        # print(vel_pos)
         f = InterpolatedUnivariateSpline(tstamp, filtered_disp, k=1)  # k=1 gives linear interpolation
         fp = f.derivative()
-        vel = [fp(i) for i in range(len(tstamp))]
-        return vel
+        vel_pos = [fp(i) for i in tstamp]
+        # p1 = filtered_disp[:-1]
+        # p2 = filtered_disp[1:]
+        # t1 = tstamp[:-1]
+        # t2 = tstamp[1:]
+        # dp = np.array(p2)-np.array(p1)
+        # dt = np.array(t2)-np.array(t1)
+        # vel_pos = [i/j for i,j in zip(dp, dt)]
+        return vel_pos
                 
     def butter_lowpass_filter(self, sample_data, cutoff, fs, order, n):
         data = sample_data[:n]
@@ -130,8 +141,17 @@ class velocity_calc:
         y = filtfilt(b, a, data)
         return y
     
+    def butter_highpass_filter(self, sample_data, cutoff, fs, order, n):
+        data = sample_data[:n]
+        nyq = 0.5 * fs  # Nyquist Frequency:
+        normal_cutoff = cutoff / nyq
+        # Get the filter coefficients 
+        b, a = butter(order, normal_cutoff, btype='high', analog=False)
+        y = filtfilt(b, a, data)
+        return y
+    
     def main(self):
-        fname = sorted(glob.glob("/home/"+os.environ.get("USERNAME")+"/catkin_ws/src/shakebot_perception/scripts/record*"))[-1]
+        fname = sorted(glob.glob("/home/"+os.environ.get("USERNAME")+"/catkin_ws/src/shakebot_perception/scripts/record*"))[-6]
         self.data = self.read_data(fname)
         posx = self.extractData("posx", self.data)
         # posx = sorted(posx, key=lambda x:x[0])
@@ -143,28 +163,40 @@ class velocity_calc:
         # accz = self.extractData("accz", self.data)
         
         # displacement processing to get velocity
-        pos_tstamp = [x[0] for x in accy]
-        pos_tbf = [x[1] for x in accy]
-        vel_pos = self.get_velocity_fpos(pos_tstamp, pos_tbf)
-        vel_pos_plot = [[i,j] for i,j in zip(pos_tstamp, vel_pos)]
+        T_pos = [x[0] for x in posx][-1]        # Sample period
+        fs_pos = 60           # Accelerometer sampling rate, Hz
+        cutoff_pos = 5       # desired cutoff frequency of the filter, Hz, slightly higher than actual 30 Hz 
+        order_pos = 2         # butterworth filter order
+        n_pos = int(T_pos * fs_pos)   # total number of samples
+        
+        pos_tstamp = [x[0] for x in posx]
+        pos_tbf = [x[1] for x in posx]
+        filtered_pos = self.butter_lowpass_filter(pos_tbf, cutoff_pos, fs_pos, order_pos, n_pos)
+        pos_plot = [[i,j] for i,j in zip(pos_tstamp[:n_pos], filtered_pos)]
+        vel_pos = self.get_velocity_fpos(pos_tstamp[:n_pos], filtered_pos)
+        vel_pos_plot = [[i,j] for i,j in zip(pos_tstamp[:n_pos], vel_pos)]
+        # vel_pos = self.get_velocity_fpos(pos_tstamp, pos_tbf)
+        # vel_pos_plot = [[i,j] for i,j in zip(pos_tstamp, vel_pos)]
         
         # acceleration processing to get velocity
-        T = [x[0] for x in accy][-1]        # Sample period
-        fs = 80           # Accelerometer sampling rate, Hz
-        cutoff = 30       # desired cutoff frequency of the filter, Hz, slightly higher than actual 30 Hz 
-        order = 2         # butterworth filter order
-        n = int(T * fs)   # total number of samples
+        T_acc = [x[0] for x in accy][-1]        # Sample period
+        fs_acc = 80           # Accelerometer sampling rate, Hz
+        cutoff_acc = 30       # desired cutoff frequency of the filter, Hz, slightly higher than actual 30 Hz 
+        order_acc = 2         # butterworth filter order
+        n_acc = int(T_acc * fs_acc)   # total number of samples
         
         acc_tstamp = [x[0] for x in accy]
         acc_tbf = [x[1] for x in accy]
-        filtered_acc = self.butter_lowpass_filter(acc_tbf, cutoff, fs, order, n)
-        acc_plot = [[i,j] for i,j in zip(acc_tstamp[:n], filtered_acc)]
-        vel_acc = self.get_velocity_facc(acc_tstamp[:n], filtered_acc)
-        vel_acc_plot = [[i,j] for i,j in zip(acc_tstamp[:n], vel_acc)]
+        filtered_acc = self.butter_lowpass_filter(acc_tbf, cutoff_acc, fs_acc, order_acc, n_acc)
+        acc_plot = [[i,j] for i,j in zip(acc_tstamp[:n_acc], filtered_acc)]
+        vel_acc = self.get_velocity_facc(acc_tstamp[:n_acc], filtered_acc)
+        vel_acc_plot = [[i,j] for i,j in zip(acc_tstamp[:n_acc], vel_acc)]
         
         # ploting data
         # self.plot([accy[:], posx[:], vel_acc_plot[:], vel_pos_plot[:]], ["acceleration", "position", "velocity_acc", "velocity_pos"])
-        self.plot([acc_plot[:], posx[:], vel_acc_plot[:], vel_pos_plot[:]], ["acceleration", "position", "velocity_acc", "velocity_pos"])
+        self.plot([acc_plot[:], pos_plot[:], vel_acc_plot[:], vel_pos_plot[:]], ["acceleration", "position", "velocity_acc", "velocity_pos"])
+        plt.plot(pos_tstamp,vel_pos)
+        plt.show()
     
 if __name__=="__main__":
     s = velocity_calc()
